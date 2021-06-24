1614a14959ec08b2d68959abf5e5237947efc841
Juan Villalba <juan.villalba08@gmail.com>
2021-06-24 15:33:26 -0300

    cambios de ubicacion de imagen

--- src/componentes/Slider/Slides.js
+++ src/componentes/Slider/Slides.js
@@ -5,22 +5,22 @@ import "./Slider.css";
 
 const slidesInfo = [
     {
-        src: "https://images.pexels.com/photos/8339774/pexels-photo-8339774.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        src: "https://images.pexels.com/photos/8339775/pexels-photo-8339775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         alt:"Project1",
         href: "https://find-your-henry-rho.vercel.app",
-        desc:"E commerce"
+        desc:"Henry Dev Job"
     },
-   /* {
+    /* {
         src: "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
         alt:"Project 2",
         href:"",
         desc:"Project 2"
     }, */
     {
-        src: "https://images.pexels.com/photos/8339775/pexels-photo-8339775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        src: "https://images.pexels.com/photos/8339774/pexels-photo-8339774.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         alt:"Project 3",
         href:"https://actitud.herokuapp.com/",
-        desc:"Henry Dev Job"
+        desc:"Ecommerce"
     },
 ]
 
