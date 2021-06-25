3ec07244fd9c90aa4f520f4bdc16b6a50e75b007
Juan Villalba <juan.villalba08@gmail.com>
2021-06-25 14:06:48 -0300

    incluidos los iconos

--- src/componentes/About/About.js
+++ src/componentes/About/About.js
@@ -2,16 +2,32 @@ import React from "react";
 import "./About.css";
 import imagenperfil from "../../media/imagenperfil.jpg"
 
-const About = ()=>{
-    return(
+const About = () => {
+    return (
         <div className="about-container">
             <div className="about-desc">
                 <h3>Let me tell you something about me</h3>
                 <p>I´m a Software Developer with abilities in backend and frontend. I´m a FullStack Developer.
-                By the way, I´m always learning a little bit more, IT is like an infinite loop 👨‍💻, don´t you think so?.</p>
+                    By the way, I´m always learning a little bit more, IT is like an infinite loop 👨‍💻, don´t you think so?.</p>
+                <div className="languages-container">
+                    <img className="une" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40" />
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40" />
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40" />
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40" />
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" alt="redux" width="40" height="40" />
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="40" height="40" />
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40" />
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40" />
+                    <img src="https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg" alt="graphql" width="40" height="40" />
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40" />
+                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="40" height="40" />
+                    <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="40" height="40" />
+  
+                </div>
             </div>
+
             <div className="about-img">
-                <img src={imagenperfil} alt="About Juan"/>
+                <img src={imagenperfil} alt="About Juan" />
             </div>
         </div>
     )
