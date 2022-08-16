b8eeb3f4e8a594f859d86122b2904243720fcc3f
Juan Villalba <juan.villalba08@gmail.com>
2022-08-16 11:21:09 -0300

    clean and push

--- src/componentes/Slider/Slides.js
+++ src/componentes/Slider/Slides.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import "./Slider.css";
 import todo from "../../media/todoapp.jpg";
 import meli from "../../media/henrymelichallenge.jpg";
-import calculadolar from "../../media/calculadolar.jpg"
-import henrydevjob from "../../media/henrydevjob.jpg"
-import henryecommerce from "../../media/henryecommerce.jpg"
+import calculadolar from "../../media/calculadolar.jpg";
+import henrydevjob from "../../media/henrydevjob.jpg";
+import henryecommerce from "../../media/henryecommerce.jpg";
+import samvilla from "../../media/samvilla.jpg";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
@@ -44,7 +45,8 @@ const slidesInfo = [
         src: henrydevjob,
         alt: "Project1",
         href: "https://find-your-henry-rho.vercel.app",
-        desc: "Henry Dev Job"
+        desc: "Henry Dev Job",
+
     },
     {
         src: todo,
@@ -67,22 +69,31 @@ const slidesInfo = [
     {
         src: calculadolar,
         alt: "Proyecto 5",
-        href: "",
+        href: "https://www.linkedin.com/posts/juanvillalba5_reactnative-expo-reactnative-activity-6909883248349188096-B7Ti?utm_source=linkedin_share&utm_medium=member_desktop_web",
         desc: "Calculadolar"
+    },
+    {
+        src: samvilla,
+        alt: "Proyecto 6",
+        href: "https://www.samvilla.com/",
+        desc: "Sam Villa"
     },
 ]
 
 
 
 const Slides = slidesInfo.map(slide => (
+    <>
     <div className="slide-container">
         <img src={slide.src} alt={slide.alt} onClick={() => alert("wait... a new feature is coming")} />
         <div className="slide-desc">
-            <a href={slide.href} rel="noreferrer" target="_blank">Ir a la app</a>
+       
+
+            <a href={slide.href} rel="noreferrer" target="_blank">Click and Go!</a>
         </div>
-        <p>Hola chango</p>
     </div>
-    
+        
+        </>
 ))
 
 
